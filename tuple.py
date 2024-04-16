890e464460652a32ecd921f8f8732f0bee30b667
Axumawityemaneh <yemanehaxumawit@gmail.com>
2024-04-16 07:18:31 +0300

    new files

---
+++ tuple.py
@@ -0,0 +1,36 @@
+# a tuple() with intergers
+my_tuple =(2,4,8,20,9)
+print(my_tuple)
+
+
+# a tuple() with mixed data type
+my_tuple =('DAS','Nairat',34,50,'Hawah','Gatchang')
+print(my_tuple)
+
+# How to access an item in a tuple / how to access an index of an item
+print(my_tuple[2])  
+print(my_tuple.index('Hawah'))
+
+# how to add an item in a tuple
+print(my_tuple)
+
+# a tuple with duplicate items
+my_tuple = ('DAS','Nairat',2,'DAS')
+print(my_tuple)
+
+
+# tuples without using parenthesis
+our_tuples ='python','JavaScript','Kotlin','Django'
+print(our_tuples)
+print(our_tuples[1])
+print(our_tuples.index('Django'))
+
+# repetition in a tuple
+my_name=("Mary",)*3
+print(my_name)
+
+# using slicing  
+print(my_tuple[0:2])
+print(our_tuples[0:3])
+
+
